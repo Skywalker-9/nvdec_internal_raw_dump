7768a9ea23c3513f58f687766ddaa9f3de8f2f39
Skywalker-9 <54350554+Skywalker-9@users.noreply.github.com>
2023-01-17 12:09:24 +0530

    Add files via upload
    $ gst-launch-1.0 filesrc location= ~/sample_720p.mp4 ! qtdemux ! h264parse ! nvdec ! fakesink
    
    Above command dumps GRAY8 output, playback using below command
    
    $ gst-launch-1.0 filesrc location= ./nvdec_filedump_1280x720.yuv ! videoparse width=1280 height=720 format=gray8 ! videoconvert ! nveglglessink

---
+++ decPerf_nvdec.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Usage: decPerf.sh <file> <num-instances>"
+  exit 0
+fi
+
+FILE=$1
+NUM_INSTANCES=$2
+
+if [ ! -f "$FILE" ]; then
+  echo "Error: Input file '$FILE' not found"
+  exit 0
+fi
+
+NUM_FRAMES=$(gst-launch-1.0 filesrc location="$FILE" ! qtdemux ! h264parse ! nvdec ! fakesink silent=0 -v 2>&1 | grep chain | wc -l)
+
+echo "Frames in file: $NUM_FRAMES"
+echo "Running $NUM_INSTANCES decode instances in parallel"
+
+CMD="time gst-launch-1.0"
+for i in `seq $NUM_INSTANCES` ; do
+  CMD="$CMD filesrc location=$FILE ! qtdemux ! h264parse ! nvdec ! fakesink"
+done
+
+OUTPUT="$(eval $CMD 2>&1)"
+if echo "$OUTPUT" | grep -i error ; then
+  exit 0
+fi
+
+TIME=$(echo "$OUTPUT" | grep ended | awk '{print $NF}')
+echo "Total time taken $TIME"
+SEC=$(echo $TIME | awk -F':' '{print $NF}')
+MIN=$(echo $TIME | awk -F':' '{print $2}')
+HR=$(echo $TIME | awk -F':' '{print $1}')
+TIME_SEC=$(echo "scale=3; ($HR * 3600) + ($MIN * 60) + ($SEC)" | bc)
+FPS=$(echo "scale=3; ($NUM_INSTANCES * $NUM_FRAMES) / $TIME_SEC" | bc)
+echo "Total decode FPS = $FPS"
+
